760ef24d41172cfc8275b83cf0b7d2bd50977e28
Adrian <adrian@github.com>
2019-05-07 23:41:49 -0400

    adding body parser

--- index.ts
+++ index.ts
@@ -1,21 +1,23 @@
-import * as dotenv from "dotenv"
+import * as dotenv from "dotenv";
+import * as bodyParser from "body-parser";
 dotenv.config();
 
 import express = require('express');
 
 const PORT = process.env.PORT || 3000;
 const app: express.Application = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 
 import {homeFunctionRoute} from "./Controllers/home"
 
 app.get('/home', async (req, res, next)=>{
-    await homeFunctionRoute(req,res,next);
+  //  await homeFunctionRoute(req,res,next);
+  
 });
 
 app.use('/home', async (req, res) => {
-
     console.log(`In the next call, I produce a name like ${req.query.firstname}`)
-
 });
 
 
